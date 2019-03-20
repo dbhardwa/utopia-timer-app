1c85064f54adc1e52868ea141abafaada30b0064
Devansh Bhardwaj <devansh@Devanshs-MacBook-Pro.local>
2019-03-20 17:34:27 -0400

    Major refactor and added a reset state

--- src/index.js
+++ src/index.js
@@ -1,113 +1,122 @@
 import "./scss/main.scss";
 import Tone from "tone";
 
-const synth = new Tone.Synth().toMaster()
-
 const app = {
   settings: {
-    states: ['default', 'timer', 'countdown'], // Possible states: 'default' --> 'timer' --> 'countdown' --> 'default'
-    currentState: 0,
+    states: ['default', 'timer', 'countdown', 'reset'], // Cyclic nature.
+    currentState: 3, // Want to initialize to state 0.
 
     startingTime: 30,
-    time: 30,
+    countdownTime: 3,
 
-    timer: 0,
+    mainTimer: 0,
     countdownTimer: 0,
   },
 
   init: function() {
     this.bindEvents();
+    this.updateState();
   },
 
   bindEvents: function() {
     const buttonHTML = document.getElementsByTagName('button')[0];
 
-    buttonHTML.addEventListener('click', function () {
+    buttonHTML.addEventListener('click', function() {
+      // De-activates button click during countdown.
       if (this.settings.states[this.settings.currentState] !== 'countdown') {
-          console.log('DONT DO SHIT');
           this.updateState();
       }
     }.bind(this));
   },
 
-  updateState: function(muteState) {
+  updateState: function() {
     let states = this.settings.states;
     let currentState = this.settings.currentState;
 
-    // Update state.
-    (states[currentState] === 'countdown') ? currentState = 0 : currentState++;
+    // Update state (cycle back if necessary).
+    (states[currentState] === 'reset') ? currentState = 0 : currentState++;
 
     // Update state in settings.
     this.settings.currentState = currentState;
 
-    // Debug...
-    console.log(states[currentState]);
-
-    this.handleClick();
+    this.render();
   },
 
-  handleClick: function() {
-    let states = this.settings.states;
-    let currentState = this.settings.currentState;
-
+  handleState: function() {
+    const synth = new Tone.Synth().toMaster();
     const timerHTML = document.getElementById('timer');
+    let time;
+
+    switch (this.settings.states[this.settings.currentState]) {
+      case 'timer':
+        time = this.settings.startingTime;
+        this.settings.mainTimer = setInterval(() => {
+          time--;
+          timerHTML.innerHTML = time;
+
+          if (time === this.settings.countdownTime) this.updateState();
+        }, 1000);
+        break;
+
+      case 'countdown':
+        clearInterval(this.settings.mainTimer);
+
+        time = this.settings.countdownTime;
+        timerHTML.innerHTML = time;
+        synth.triggerAttackRelease('C4', '8n');
+
+        this.settings.countdownTimer = setInterval(() => {
+          time--;
+          timerHTML.innerHTML = time;
+
+          if (time === 0) {
+            synth.triggerAttackRelease('C5', '4n');
+            clearInterval(this.settings.countdownTimer);
+            this.updateState();
+          } else
+            synth.triggerAttackRelease('C4', '8n');
+        }, 1000);
+        break;
+    }
+  },
+
+  render() {
     const bodyHTML = document.getElementsByTagName('body')[0];
+    const timerHTML = document.getElementById('timer');
     const subheadHTML = document.getElementById('subhead');
     const buttonHTML = document.getElementsByTagName('button')[0];
 
-    switch (states[currentState]) {
+    switch (this.settings.states[this.settings.currentState]) {
       case 'default':
         bodyHTML.className = 'primaryBackground';
-        subheadHTML.className = 'secondaryColor';
         timerHTML.className = 'hidden'; // Hide timer on default screen.
+        subheadHTML.className = 'secondaryColor';
         buttonHTML.innerHTML = 'START TIMER';
-
-        // Update time.
-        this.settings.time = this.settings.startingTime;
-        timerHTML.innerHTML = this.settings.time;
-
         break;
 
       case 'timer':
         bodyHTML.className = 'secondaryBackground';
+        timerHTML.className = 'timerStateTimer';
+        timerHTML.innerHTML = this.settings.startingTime;
         subheadHTML.className = 'primaryColor';
-        timerHTML.className = '';
         buttonHTML.innerHTML = 'VOTE';
-
-        this.settings.timer = setInterval(() => {
-          this.settings.time--;
-          timerHTML.innerHTML = this.settings.time;
-
-          if (this.settings.time === 3) {
-            this.updateState();
-          };
-        }, 1000);
-
+        this.handleState();
         break;
 
       case 'countdown':
-        clearInterval(this.settings.timer);
+        timerHTML.className = 'primaryColor';
+        buttonHTML.className = 'buttonInset'
+        this.handleState();
+        break;
 
-        this.settings.time = 3;
-        timerHTML.innerHTML = this.settings.time;
-        synth.triggerAttackRelease('C4', '8n');
-
-        this.settings.countdownTimer = setInterval(() => {
-          this.settings.time--;
-          timerHTML.innerHTML = this.settings.time;
-
-          if (this.settings.time === 0) {
-            synth.triggerAttackRelease('C5', '4n');
-            clearInterval(this.settings.countdownTimer);
-            this.updateState();
-          } else {
-            synth.triggerAttackRelease('C4', '8n');
-          }
-        }, 1000);
-
+      case 'reset':
+        bodyHTML.className = 'primaryBackground';
+        timerHTML.innerHTML = 'VOTE';
+        timerHTML.className = 'timerStateReset';
+        buttonHTML.innerHTML = 'RESET';
+        buttonHTML.className = '';
         break;
     }
-
   }
 };
 
